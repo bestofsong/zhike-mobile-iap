1f8c147bc00b1c4aedb226015ac444da5f742501
wansong <betterofsong@gmail.com>
2018-01-22 19:54:04 +0800

    1.1.1: refactor, using rc to communicate result

--- iap.js
+++ iap.js
@@ -13,13 +13,9 @@ const promisify = (fn, receiver) => (
 );
 
 const { InAppUtils, ZKFileUtils } = NativeModules;
-const purchaseProduct = promisify(InAppUtils.purchaseProduct, { thisArg:InAppUtils });
-const loadProducts = promisify(InAppUtils.loadProducts, { thisArg:InAppUtils });
+const purchaseProduct = promisify(InAppUtils.purchaseProduct, { thisArg: InAppUtils });
+const loadProducts = promisify(InAppUtils.loadProducts, { thisArg: InAppUtils });
 
-
-function identity(whatever) {
-  return whatever;
-}
 
 function icloudAvailable() {
   return new Promise((resolve, reject) => {
@@ -61,95 +57,153 @@ export default class Iap {
   }
 
   async getProduct() {
-    if (this._getProductPromise) {
-      return await this._getProductPromise;
+    if (this.getProductPromise) {
+      const p = await this.getProductPromise;
+      return p;
     }
-    this._getProductPromise = loadProducts([this.ctx.productId])
-    .then((products) => {
-      return products && products[0];
-    })
-    .catch((e) => {
-      console.error('failed to loadProduct, id: ', this.ctx);
-      return null;
-    });
+    this.getProductPromise = loadProducts([this.ctx.productId])
+      .then((products) => {
+        return products && products[0];
+      })
+      .catch((e) => {
+        console.error('failed to loadProduct, id: ', this.ctx, e);
+        return Promise.reject(e);
+      });
 
-    return await this.getProduct();
+    const ret = await this.getProduct();
+    return ret;
   }
 
-  // callback([appStoreProduct, paymentInfo, isCached]), return Promise<[bool, passdata]>, indicate success fail
+  // callback([appStoreProduct, paymentInfo, isCached]),
+  // return Promise<[bool, passdata]>, indicate success fail
   // return: [success: bool, passdata]
   async purchase(callback) {
+    const { iapRecordVendor, productId, isLoggedIn } = this.ctx;
+    const { savePayRecord, removePayRecord, getPayRecord } = iapRecordVendor;
+    // 先检查已保存的记录
+    let rec = null;
     try {
-      const { iapRecordVendor, productId } = this.ctx;
-      const { savePayRecord, removePayRecord, getPayRecord } = iapRecordVendor;
-
-      // 先检查已支付的记录
-      const rec = await getPayRecord.call(iapRecordVendor, productId);
-      if (rec) {
-        if (rec.length !== 2) {
-          throw new Error('invalid pay record');
-        }
-        const ret = await Promise.resolve(callback([...rec, true]));
-        if (!ret[0]) {
-          return ret;
-        }
-        // 已有的记录，提交成功就删掉
-        await removePayRecord.call(iapRecordVendor, rec);
-        return ret;
-      }
-
-      const appStoreProduct = await this.getProduct();
-      if (!appStoreProduct) {
-        const ret = await callback(null);
-        return ret;
-      }
-
-      const paymentInfo = await this._iapPay();
-      const [success, passdata] = await Promise.resolve(callback([appStoreProduct, paymentInfo]));
-      if (success) {
-        return [success, passdata];
-      }
-
-      // 新的记录，提交失败了，保存
-      await savePayRecord.call(iapRecordVendor, appStoreProduct, paymentInfo);
-      return [false, passdata];
+      rec = await getPayRecord.call(iapRecordVendor, productId);
     } catch (e) {
-      console.error('purchase failed, error: ', e);
-      return [false, e];
+      console.error('getPayRecord failed, should not happen: ', e);
+    }
+
+    if (rec) {
+      if (rec.length !== 2) {
+        throw new Error('invalid pay record');
+      }
+
+      if (!isLoggedIn) {
+        return { rc: 'RC_IAP_DID_SAVE_REC' };
+      }
+
+      let ret = null;
+      try {
+        ret = await callback({ product: rec[0], payment: rec[1], isRestored: true });
+      } catch (e) {
+        console.error(e);
+        return { rc: 'RC_IAP_CALLBACK', error: e };
+      }
+
+      if (ret.rc !== 'RC_OK') {
+        return { rc: 'RC_IAP_CALLBACK' };
+      }
+
+      try {
+        await removePayRecord.call(iapRecordVendor, rec);
+      } catch (e) {
+        // fixme: any better way to handle this
+        console.error('removePayRecord failed, should not happen: ', e);
+      }
+      return ret;
+    }
+
+    let appStoreProduct = null;
+    try {
+      appStoreProduct = await this.getProduct();
+    } catch (e) {
+      console.error(e);
+      return { rc: 'RC_IAP_GET_PRODUCT', error: e };
+    }
+
+    if (!appStoreProduct) {
+      return { rc: 'RC_IAP_GET_PRODUCT' };
+    }
+
+    let payment = null;
+    try {
+      payment = await this.iapPay();
+    } catch (e) {
+      console.error(e);
+      return { rc: 'RC_IAP_PURCHASE', error: e };
+    }
+
+    if (!payment) {
+      return { rc: 'RC_IAP_PURCHASE' };
+    }
+
+    let callbackRes = null;
+    let errorRc = '';
+    try {
+      callbackRes = await callback({ product: appStoreProduct, payment });
+    } catch (e) {
+      console.error(e);
+      if (e && e.rc) {
+        errorRc = e.rc;
+      }
+    }
+
+    const { rc, ...rest } = callbackRes || {};
+    if (isLoggedIn && rc === 'RC_OK') {
+      return callbackRes;
+    }
+
+    // 提交失败了，或未登录
+    try {
+      await savePayRecord.call(iapRecordVendor, appStoreProduct, payment);
+      return {
+        ...rest,
+        rc: isLoggedIn ? (rc || errorRc || 'RC_IAP_CALLBACK') : 'RC_IAP_DID_SAVE_REC',
+      };
+    } catch (e) {
+      console.error(e);
+      return { rc: 'RC_IAP_SAVE_REC', error: e };
     }
   }
 
-  async _iapPay(alertIfNoUser = true) {
-    const isLoggedIn = this._isLoggedIn();
+  async iapPay(alertIfNoUser = true) {
+    const { isLoggedIn } = this.ctx;
     if (!isLoggedIn && alertIfNoUser) {
-      return await new Promise((resolve, reject) => {
+      const iapPayResult = await new Promise((resolve, reject) => {
         Alert.alert(
           '友情提示',
-          '您正在未登录的情况下购买课程。\
-          支付完成后，您可以在本机登录智课账号以便提交订单。\
-          如果您需要在其他手机提交订单，请确保已经使用您的Apple Id登录iCloud。',
+          '您正在未登录的情况下购买课程。支付完成后，您可以在本机登录智课账号以便提交订单。如果您需要在其他手机提交订单，请确保已经使用您的Apple Id登录iCloud。',
           [
             {
               text: '先去登录',
               onPress: () => {
                 reject('支付已取消');
-                this._goLogin();
+                this.goLogin();
               },
             },
             {
               text: '继续',
               onPress: () => {
                 icloudAvailable()
-                .then((avail) => {
-                  if (avail) {
-                    resolve(this._iapPay(false));
-                  } else {
-                    reject('请打开iCloud存储权限，然后重新尝试报名。');
-                  }
-                })
-                .catch((error) => {
-                  reject(error);
-                });
+                  .catch((e) => {
+                    console.error('icloudAvailable error: ', e);
+                    return false;
+                  })
+                  .then((avail) => {
+                    if (avail) {
+                      resolve(this.iapPay(false));
+                    } else {
+                      reject('请打开iCloud存储权限，然后重新尝试报名。');
+                    }
+                  })
+                  .catch((error) => {
+                    reject(error);
+                  });
               },
             },
             {
@@ -161,17 +215,15 @@ export default class Iap {
           ]
         );
       });
+
+      return iapPayResult;
     }
 
-    return await purchaseProduct(this.ctx.productId);
+    const res = await purchaseProduct(this.ctx.productId);
+    return res;
   }
 
-  // convenient
-  _isLoggedIn() {
-    return this.ctx && !!this.ctx.isLoggedIn;
-  }
-
-  _goLogin() {
+  goLogin() {
     const { login } = this.ctx;
     if (login) {
       login(this.ctx);
